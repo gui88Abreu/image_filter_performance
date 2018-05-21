0b275f05f6f492979db891063f17cb1c96e79c3f
Guilherme Abreu <guiii88@outlook.com>
2018-05-21 13:01:59 -0300

    adding test script

---
+++ test.py
@@ -0,0 +1,26 @@
+'''Test Script '''
+
+from os import system
+import sys
+
+if len(sys.argv) != 2:
+  quit()  
+
+images_directory = "images"
+program_target = sys.argv[1]
+temp_file = "temp"
+
+system('ls ' + images_directory + '>' + temp_file)
+
+fl = open(temp_file, 'r')
+test_list = fl.readlines()
+fl.close
+
+if len(test_list) == 0:
+  quit('There is no test files')
+
+for test in test_list:
+  print('Image: ' + test)
+  system('time ./'+program_target+'<'+test)
+
+system('rm -f ' + temp_file)
