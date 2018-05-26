f98b1bbdf90007f22abdffb290b99adc84ae2ea0
Guilherme Abreu <guiii88@outlook.com>
2018-05-26 02:13:25 -0300

    Add comments

--- test.py
+++ test.py
@@ -1,15 +1,30 @@
-'''Test Script '''
+###########################################################################################################
+###########################################################################################################
 
-from os import system
-import sys
+# Here are the imports needed
+
+from os import system #execute commands on command line
+import sys #catch arguments from the command line that runs this script
+
+###########################################################################################################
+###########################################################################################################
+
+# Here are the function statements
 
 def yield_table(time_file, Name):
+  ''' 
+  Yield a csv table with datas that were colected and stored on time_file.
+  String *Name must have something about the datas, example: Thread Method or just Thread.
+  '''
+  #Store all datas yielded previously in make_test on a list
   file = open(time_file, 'r')
   line = file.readlines()
   file.close()
 
+  #Erase time_file
   system('rm -f ' + time_file)
 
+  # Actually yield a csv table
   planilha = open('Test_Result.csv', 'a')
   planilha.write(Name + '\n')
   planilha.write('Test,Usr time[s],Real time[s]\n')
@@ -21,6 +36,11 @@ def yield_table(time_file, Name):
   planilha.close()
 
 def make_test(test_list, program_target, mode, time_file):
+  '''
+  It does test program_target with the test_list and store the results on time_file.
+  *mode must be assigned with '0' to execute program with threads or with '1' to execute with processes.
+  '''
+
   for i in range(len(test_list)):
     test_list[i] = test_list[i].replace('\n', '')
     print(60*'-')
@@ -28,29 +48,42 @@ def make_test(test_list, program_target, mode, time_file):
     system('./'+program_target + ' ' + test_list[i] + ' ' + mode + ' >>' + time_file)
   print(60*'-')
 
+###########################################################################################################
+###########################################################################################################
+
+# Here begin the main
+
 if len(sys.argv) != 2:
-  quit()  
+  quit('It must be passed as argument just the program name that will be tested')
 
 images_directory = "images"
 program_target = sys.argv[1]
 temp_file = "temp"
 time_file = "time"
 
+#Catch all file names in image_directory and store on temp_file
 system('ls ' + images_directory + '>' + temp_file)
 
+#Store all names found in image_directory on a list
 fl = open(temp_file, 'r')
 test_list = fl.readlines()
 fl.close
+
+#Erase temp_file
 system('rm -f ' + temp_file)
 
 if len(test_list) == 0:
   quit('There is no test files')
 
+#Runs program_target with threads and store the results on time_file
 make_test(test_list, program_target, '0', time_file)
 yield_table(time_file, 'Thread')
 
 #When the part of the program that apply blur with processes be ready, descomment the code below
 '''
+#Runs program_target with processes and store the results on time_file
 make_test(test_list, program_target, '1', time_file)
 yield_table(time_file, 'Process')
 '''
+###########################################################################################################
+###########################################################################################################
